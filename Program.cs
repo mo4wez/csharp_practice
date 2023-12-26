0eb9d70da83a82b00933803594fbb20765ee8bfa
moawezz <moawez.de@gmail.com>
2023-12-26 13:46:07 +0330

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace StringsInCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = OneToTenWithOutInput();
+            string[] SplitedResult = result.Split("|");
+            for (int i = 0; i < SplitedResult.Length; i++)
+            {
+                Console.WriteLine(SplitedResult[i]);
+            }
+        }
+
+        static string OneToTenWithOutInput() 
+        {
+            string result = "Finish";
+            int j = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                j += i;
+                result += Convert.ToString(i);
+            }
+            return result + "|" + "sum:" + j;
+        }
+    }
+}
